2be91a6562499190a550f9bb73a5e0ea342e1fe9
youngwishes <d.fedorov@ylab.team>
2024-07-03 00:11:28 +0300

    init

--- src/orders/domains/query.py
+++ src/orders/domains/query.py
@@ -22,19 +22,13 @@ class OrderQueriesResult(IQueryResult):
 class OrderQueryHandler(IQueryHandler):
     @in_journal
     async def handle(self, queries: OrderQueries) -> OrderQueriesResult:
-        async with self.pools[0].acquire() as first_connection:
-            async with self.pools[1].acquire() as second_connection:
-                self.update_query_loggers(
-                    queries, (first_connection, second_connection)
-                )
-                result = await asyncio.gather(
-                    *[
-                        self.provider.make(query=sql, connection=conn)
-                        for conn, sql in zip(
-                            (first_connection, second_connection),
-                            (queries.first, queries.second),
-                        )
-                    ],
-                    return_exceptions=True,
-                )
+        async with self.pools[0].acquire() as first_connection, self.pools[
+            1
+        ].acquire() as second_connection:
+            self.update_query_loggers(queries, (first_connection, second_connection))
+            result = await asyncio.gather(
+                self.provider.make(connection=first_connection, query=queries.first),
+                self.provider.make(connection=second_connection, query=queries.second),
+                return_exceptions=True,
+            )
         return OrderQueriesResult(first=result[0], second=result[1])
--- src/orders/gateways/providers/query.py
+++ src/orders/gateways/providers/query.py
@@ -3,7 +3,7 @@ from orders.abstract.provider import IQueryProvider
 
 
 class OrderQueryProvider(IQueryProvider):
-    async def make(self, query: str, connection: Connection) -> None:
+    async def make(self, query: str, connection: Connection) -> list:
         async with connection.transaction(isolation=self.isolation):
             cur = await connection.cursor(query)
             return await cur.fetch(10)
